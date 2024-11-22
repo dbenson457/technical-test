99db3061d6d63eda35687bef56dc05ac4331b05c
dbenson457 <dbenson457@hotmail.co.uk>
2024-11-22 13:27:59 +0000

    Installed React and Tailwind CSS with inital Fork and Docker configuartion

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.jsx",  // Added this line for tailwind to be used in React files 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
